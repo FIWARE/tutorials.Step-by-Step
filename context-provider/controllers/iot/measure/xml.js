a73fea5d74cfbfe197c966b87e740fd1fbf6d66e
Jason Fox <jason.fox@fiware.org>
2019-11-21 18:43:03 +0000

    XML Measure Support

--- context-provider/controllers/iot/measure/xml.js
+++ context-provider/controllers/iot/measure/xml.js
@@ -22,22 +22,23 @@ const IOT_AGENT_URL =
 /* global MQTT_CLIENT */
 
 
-// This processor sends ultralight payload northbound to
+// This processor sends XML payloads northbound to
 // the southport of the IoT Agent and sends measures
 // for the motion sensor, door and lamp.
 
-// Ultralight 2.0 is a lightweight text based protocol aimed to constrained
-// devices and communications
-// where the bandwidth and device memory may be limited resources.
-//
-// A device can report new measures to the IoT Platform using an HTTP GET request to the /iot/d path with the following query parameters:
-//
-//  i (device ID): Device ID (unique for the API Key).
-//  k (API Key): API Key for the service the device is registered on.
-//  t (timestamp): Timestamp of the measure. Will override the automatic IoTAgent timestamp (optional).
-//  d (Data): Ultralight 2.0 payload.
-//
-// At the moment the API key and timestamp are unused by the simulator.
+function ultralightToXML(key, deviceId, state) {
+  const keyValuePairs = state.split('|');
+  let payload ="";
+
+  payload = payload + '<measure device="' + deviceId + '" key="' + key + '">\n';
+  for (let i = 0; i < keyValuePairs.length; i = i + 2) {
+   payload = payload +  '<' + keyValuePairs[i] + ' value="' + keyValuePairs[i+1] + '"/>\n';
+  }
+  payload = payload + '</measure>';
+  return payload;
+}
+
+
 
 class XMLMeasure {
   constructor(headers) {
@@ -47,22 +48,23 @@ class XMLMeasure {
 
   // measures sent over HTTP are POST requests with params
   sendAsHTTP(deviceId, state) {
+    const payload = ultralightToXML(DEVICE_API_KEY, deviceId, state);
     const options = {
       method: 'POST',
       url: IOT_AGENT_URL,
-      qs: { k: DEVICE_API_KEY, i: deviceId },
       headers: this.headers,
-      body: state
+      body: payload
     };
     const debugText =
-      'POST ' + IOT_AGENT_URL + '?i=' + options.qs.i + '&k=' + options.qs.k;
+      'POST ' + IOT_AGENT_URL 
 
     request(options, error => {
       if (error) {
         debug(debugText + ' ' + error.code);
       }
     });
-    SOCKET_IO.emit('http', debugText + '  ' + state);
+
+    SOCKET_IO.emit('http',  debugText  + "<br/>" + ' ' + payload.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\n/g, "<br/>"));
   }
 
   // measures sent over MQTT are posted as topics (motion sensor, lamp and door)
