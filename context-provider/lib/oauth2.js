9757785f47a52212255cdd418433219f9d5a7100
Jason Fox <jason.fox@fiware.org>
2020-10-29 17:00:46 +0100

    Buffer() is deprecated due to security and usability issues.

--- context-provider/lib/oauth2.js
+++ context-provider/lib/oauth2.js
@@ -45,7 +45,7 @@ exports.OAuth2.prototype._getAccessTokenUrl = function () {
 // e.g. Authorization: Bearer <token>  # Build "Bearer <token>"
 exports.OAuth2.prototype.buildAuthHeader = function () {
     const key = this._clientId + ':' + this._clientSecret;
-    const base64 = new Buffer(key).toString('base64');
+    const base64 = new Buffer.from(key).toString('base64');
     return this._authMethod + ' ' + base64;
 };
 
